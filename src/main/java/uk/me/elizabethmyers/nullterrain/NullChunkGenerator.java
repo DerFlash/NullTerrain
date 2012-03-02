669503e05aaaf05e482cd05173c6cb858d5af68e
DerFlash <bte@freenet.de>
2012-03-02 13:29:02 +0100

    updated for bukkit R5

--- src/main/java/uk/me/elizabethmyers/nullterrain/NullChunkGenerator.java
+++ src/main/java/uk/me/elizabethmyers/nullterrain/NullChunkGenerator.java
@@ -1,12 +1,8 @@
 package uk.me.elizabethmyers.nullterrain;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.Random;
 import org.bukkit.Location;
-import org.bukkit.generator.BlockPopulator;
 import org.bukkit.generator.ChunkGenerator;
-import org.bukkit.Material;
 import org.bukkit.World;
 
 public class NullChunkGenerator extends ChunkGenerator
@@ -19,7 +15,6 @@ public class NullChunkGenerator extends ChunkGenerator
 	@Override
 	public Location getFixedSpawnLocation(World world, Random random)
 	{
-		// Sane default --Elizabeth
 		return new Location(world, 0, 64, 0);
 	}
 }
